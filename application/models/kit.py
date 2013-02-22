7a9c1cfbb676a17a38d1117b88d088bd6747e654
raindrop-aqua <raindrop.aqua@gmail.com>
2013-02-22 12:41:43 +0900

    _execute_service_post impl.

--- application/models/kit.py
+++ application/models/kit.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-import codecs
 
 import json
 import logging
 from google.appengine.api import urlfetch
+
 from application.utils import const
 
 
@@ -17,14 +17,33 @@ def search(author_id=None, category_id=None, tags=None):
     }
     data = json.dumps(request)
     url = const.WEB_SERVICE_URL_KIT_SEARCH
-    ret = _execute_service(url, data)
+    ret = _execute_service_post(url, data)
     if ret and ret.get("response").get("result") == const.RESPONSE_RESULT_SUCCESSFUL:
         return ret.get("response").get("substance").get("items")
     else:
-        return []
+        return None
 
 
-def _execute_service(url, data):
+def read(kit_id):
+    url = const.WEB_SERVICE_URL_KIT_READ + kit_id
+    ret = _execute_service_get(url)
+    if ret and ret.get("response").get("result") == const.RESPONSE_RESULT_SUCCESSFUL:
+        return ret.get("response").get("substance")
+    else:
+        return None
+
+
+def _execute_service_get(url):
+    try:
+        res = urlfetch.fetch(url=url, deadline=10)
+        if res.status_code == 200:
+            return json.loads(res.content, "utf-8")
+    except Exception, ex:
+        logging.error(ex.message)
+    return None
+
+
+def _execute_service_post(url, data):
     try:
         method = urlfetch.POST
         headers = {"Content-Type": "application/json"}
@@ -34,3 +53,17 @@ def _execute_service(url, data):
     except Exception, ex:
         logging.error(ex.message)
     return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
