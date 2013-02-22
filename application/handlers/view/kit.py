52e0adce8688ac2dbfeb4edaa7617d7254998432
raindrop-aqua <raindrop.aqua@gmail.com>
2013-02-22 12:39:40 +0900

    kit_detail impl.

--- application/handlers/view/kit.py
+++ application/handlers/view/kit.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from flask import Module, render_template, redirect
 from application.models import kit
-from flask import Module, render_template
 
 
 kit_view = Module(__name__)
@@ -12,6 +12,10 @@ def kit_search():
     return render_template("/kit/search.html", kits=kit.search())
 
 
-@kit_view.route("/kit/<kit_id>")
+@kit_view.route("/kit/detail/<kit_id>")
 def kit_detail(kit_id):
-    return render_template("/kit/detail.html")
+    kit_data = kit.read(kit_id)
+    if kit_data:
+        return render_template("/kit/detail.html", kit=kit_data)
+    else:
+        return redirect("/kit")
